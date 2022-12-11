cd015186d99d6b8a63ce9ba1c5074f00844969fe
Cevher Dogan <cevherd@gmail.com>
2022-12-11 12:48:43 -0500

    Initial visit

---
+++ projects/Calculate_age/rit.sh
@@ -0,0 +1,9 @@
+# input/output ornegi icin kullanilabilir
+# cok eksigi var tabii ki, yanlislari algilamada filan
+
+pip install -r requirements.txt
+pip list 
+pipdeptree
+
+python calculate.py
+
---
+++ projects/Calculate_age/setv.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.pvenv
+if [ -z  ~/.pvenv/python-mini-projects ]; then
+  echo "does not exist.. creating... ~/.pvenv/python-mini-projects"
+  python3 -m pip install --upgrade pip
+  python3 -m venv ~/.pvenv/python-mini-projects
+fi
+echo source ~/.pvenv/python-mini-projects/bin/activate 
+
+rit 2>&1 | tee pipinstall.log
+
+# deactivate
