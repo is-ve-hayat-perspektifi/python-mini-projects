fbaa9c635c5c9d4fdac57e31f6b04b16b1de7b63
Cevher Dogan <cevherd@gmail.com>
2022-12-11 14:03:54 -0500

    Very basic tk ui to get input/output from local

---
+++ projects/Convert_JPEG_to_PNG/rit.sh
@@ -0,0 +1,9 @@
+# General references of essential dependencies
+pip install -r requirements.txt
+# pip list 
+# pipdeptree
+
+python cnv_jpeg_png_test.py
+python cnv_tkui_example.py # enter input/output file using tk UI 
+
+
---
+++ projects/Convert_JPEG_to_PNG/setv.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.pvenv
+if [ -z  ~/.pvenv/python-mini-projects ]; then
+  echo "does not exist.. creating... ~/.pvenv/python-mini-projects"
+  python3 -m pip install --upgrade pip
+  python3 -m venv ~/.pvenv/python-mini-projects
+fi
+echo source ~/.pvenv/python-mini-projects/bin/activate 
+
+rit 2>&1 | tee /tmp/pipinstall.log
+
+cat /tmp/pipinstall.log
+rm /tmp/pipinstall.log
+
+# deactivate
